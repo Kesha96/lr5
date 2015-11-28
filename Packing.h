0f0f740248052b03a115f2faa66389a448086859
Kesha96 <leonova357@gmail.com>
2015-11-28 18:48:44 +0300

    Create Packing.h

---
+++ Packing.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <String>
+using namespace std;
+class Packing
+{
+	string material;
+
+public:
+	Packing()
+	{
+		cout << "Constructor Packing" << endl;
+	}
+	~Packing()
+	{
+		cout << "Destr Packing" << endl;
+	}
+	void SetMaterial(string s)
+	{
+		material = s;
+	}
+	string GetMaterial()
+	{
+		return material;
+	}
+};
